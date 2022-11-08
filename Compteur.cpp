6a57eecf5e9ba222e0f5ebe277b2a1ab9f7983f0
Laurent Giustignano <laurent.giustignano@free.fr>
2022-11-08 22:33:31 +0100

    Mieux mais pas top

--- Compteur.cpp
+++ Compteur.cpp
@@ -5,5 +5,7 @@
 #include "Compteur.h"
 
 Compteur::Compteur(QWidget *parent) : QWidget(parent) {
-
+    valeurCompteur = 0;
+    plusUn = new QPushButton("Plus Un", this);
+    affichageCompteur = new QLabel("valeur compteur : ", this);
 }
--- Compteur.h
+++ Compteur.h
@@ -5,12 +5,19 @@
 #ifndef QTCOMPTEUR_COMPTEUR_H
 #define QTCOMPTEUR_COMPTEUR_H
 
+#include <QLabel>
+#include <QPushButton>
 #include <QWidget>
 
 class Compteur : public QWidget
 {
 public:
     Compteur(QWidget *parent = nullptr);
+
+private:
+    QLabel *affichageCompteur;
+    QPushButton *plusUn;
+    short valeurCompteur;
 };
 
 #endif //QTCOMPTEUR_COMPTEUR_H
