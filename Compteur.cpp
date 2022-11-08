fa85d4f20d0ff84fba8e9914d6600871befd3cea
Laurent Giustignano <laurent.giustignano@free.fr>
2022-11-08 22:30:18 +0100

    Le socle d'une application

---
+++ Compteur.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Laurent Giustignano on 08/11/2022.
+//
+
+#include "Compteur.h"
+
+Compteur::Compteur(QWidget *parent) : QWidget(parent) {
+
+}
---
+++ Compteur.h
@@ -0,0 +1,16 @@
+//
+// Created by Laurent Giustignano on 08/11/2022.
+//
+
+#ifndef QTCOMPTEUR_COMPTEUR_H
+#define QTCOMPTEUR_COMPTEUR_H
+
+#include <QWidget>
+
+class Compteur : public QWidget
+{
+public:
+    Compteur(QWidget *parent = nullptr);
+};
+
+#endif //QTCOMPTEUR_COMPTEUR_H
